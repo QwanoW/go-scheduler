ecc2b5d9c6fc2929cd5b9b9e78d524cf3ac1c036
Qwano <domocristalix@gmail.com>
2023-11-12 20:05:04 +0500

    feat: keep alive for replit

--- cmd/app.go
+++ cmd/app.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 	"private/scheduler/internal"
 )
 
 func main() {
+	// keep app alive
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "ALIVE")
+		if err != nil {
+			return
+		}
+	})
+
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// start bot
 	internal.StartBot()
 }
